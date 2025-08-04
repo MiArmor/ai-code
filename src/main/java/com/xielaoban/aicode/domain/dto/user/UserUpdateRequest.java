2ac57fb0d43cd53cdddde9ca7092dd53798faf88
解哲辉 <14881796+xie-zhehui@user.noreply.gitee.com>
2025-08-04 11:25:08 +0800

    用户管理模块 1.用户的增删查改 2.用户权限管理（全局校验AOP+注解） 3.用户登录验证 4.Mybatis Flex代码生成器

---
+++ src/main/java/com/xielaoban/aicode/domain/dto/user/UserUpdateRequest.java
@@ -0,0 +1,37 @@
+package com.xielaoban.aicode.domain.dto.user;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+public class UserUpdateRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7506184322226497417L;
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 用户昵称
+     */
+    private String userName;
+
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+
+    /**
+     * 简介
+     */
+    private String userProfile;
+
+    /**
+     * 用户角色：user/admin
+     */
+    private String userRole;
+}
---
+++ src/main/java/com/xielaoban/aicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.xielaoban.aicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.xielaoban.aicode.domain.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/MiArmor">Mi_amor</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
