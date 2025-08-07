889f607c4f08eed0a2091d0a7d898fcaa498be4f
解哲辉 <14881796+xie-zhehui@user.noreply.gitee.com>
2025-08-07 20:16:15 +0800

    第六部分 对话历史 1）新增对话历史（用户和AI） 2）关联删除（同时删除应用和对话） 3）游标查询（解决传统分页查询的弊端） 4）管理员查询所有应用对话历史

--- src/main/java/com/xielaoban/aicode/service/impl/AppServiceImpl.java
+++ src/main/java/com/xielaoban/aicode/service/impl/AppServiceImpl.java
@@ -13,6 +13,7 @@ import com.xielaoban.aicode.core.AiCodeGeneratorFacade;
 import com.xielaoban.aicode.domain.dto.app.AppQueryRequest;
 import com.xielaoban.aicode.domain.entity.App;
 import com.xielaoban.aicode.domain.entity.User;
+import com.xielaoban.aicode.domain.enums.ChatHistoryMessageTypeEnum;
 import com.xielaoban.aicode.domain.vo.app.AppVO;
 import com.xielaoban.aicode.domain.vo.user.UserVO;
 import com.xielaoban.aicode.exception.BusinessException;
@@ -20,11 +21,14 @@ import com.xielaoban.aicode.exception.ErrorCode;
 import com.xielaoban.aicode.exception.ThrowUtils;
 import com.xielaoban.aicode.mapper.AppMapper;
 import com.xielaoban.aicode.service.AppService;
+import com.xielaoban.aicode.service.ChatHistoryService;
 import com.xielaoban.aicode.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.io.File;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,15 +41,18 @@ import java.util.stream.Collectors;
  *
  * @author <a href="https://github.com/MiArmor">Mi_amor</a>
  */
+@Slf4j
 @Service
 public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppService{
 
     private final UserService userService;
     private final AiCodeGeneratorFacade aiCodeGeneratorFacade;
+    private final ChatHistoryService chatHistoryService;
 
-    public AppServiceImpl(UserService userService, AiCodeGeneratorFacade aiCodeGeneratorFacade) {
+    public AppServiceImpl(UserService userService, AiCodeGeneratorFacade aiCodeGeneratorFacade, ChatHistoryService chatHistoryService) {
         this.userService = userService;
         this.aiCodeGeneratorFacade = aiCodeGeneratorFacade;
+        this.chatHistoryService = chatHistoryService;
     }
 
     @Override
@@ -126,8 +133,24 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         if (codeGenTypeEnum == null) {
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "不支持的代码生成类型");
         }
-        // 5. 调用 AI 生成代码
-        return aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
+        // 5. 通过校验后，添加用户消息到对话历史
+        chatHistoryService.addChatMessage(appId, message, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
+        // 6. 调用 AI 代码生成器生成代码
+        Flux<String> genResult = aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
+        // 7. 收集AI响应内容，保存到数据库中
+        StringBuilder aiResponse = new StringBuilder();
+        return genResult.map( str -> {
+            // 收集ai生成内容
+            aiResponse.append(str);
+            return str;}
+        ).doOnComplete( () -> {
+            // 流式响应完成后，保存到对话历史
+            chatHistoryService.addChatMessage(appId, aiResponse.toString(), ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
+        }).doOnError( error -> {
+            // 流式响应出错，保存到对话历史，记录错误消息
+            String errorMessage = "AI回复失败：" + error.getMessage();
+            chatHistoryService.addChatMessage(appId, error.getMessage(), ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
+        });
     }
 
 
@@ -176,7 +199,24 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         return String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
     }
 
-
-
-
+    /**
+     * 删除应用时，关联删除对话历史
+     *
+     * @param id 应用id
+     * @return 是否成功
+     */
+    @Override
+    public boolean removeById(Serializable id) {
+        ThrowUtils.throwIf(id == null || (Long) id <= 0, ErrorCode.PARAMS_ERROR, "应用 ID 不能为空");
+        Long appId = Long.valueOf(id.toString());
+        // 1. 先删除应用的对话历史
+        try {
+            chatHistoryService.deleteByAppId((Long) id);
+        } catch (Exception e) {
+            //记录日志，但继续删除应用
+            log.error("删除应用对话历史失败: {}", e.getMessage());
+        }
+        // 2. 删除应用
+        return super.removeById(id);
+    }
 }
